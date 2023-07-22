0fd18c9023ce188382fec10b567e5275e0f86883
acrylic-style <me@acrylicstyle.xyz>
2023-07-23 03:22:27 +0900

    update to spigot 1.20.1

--- src/main/java/xyz/acrylicstyle/mentions/Mentions.java
+++ src/main/java/xyz/acrylicstyle/mentions/Mentions.java
@@ -2,15 +2,13 @@ package xyz.acrylicstyle.mentions;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import util.ICollectionList;
-import xyz.acrylicstyle.tomeito_api.TomeitoAPI;
-import xyz.acrylicstyle.tomeito_api.sounds.Sound;
 
 public class Mentions extends JavaPlugin implements Listener {
     @Override
@@ -20,20 +18,20 @@ public class Mentions extends JavaPlugin implements Listener {
 
     @EventHandler(priority = EventPriority.LOW)
     public void onAsyncPlayerChat(AsyncPlayerChatEvent e) {
-        ICollectionList.asList(e.getMessage().split("\\s+")).forEach(s -> {
+        for (String s : e.getMessage().split("\\s+")) {
             if (s.equals("@everyone")) { // @everyone, mentions everyone (aqua color)
-                if (!e.getPlayer().hasPermission("mentions.everyone")) return;
-                e.setMessage(e.getMessage().replaceAll("@everyone", "" + ChatColor.AQUA + ChatColor.UNDERLINE + ChatColor.BOLD + "@everyone" + ChatColor.RESET + ChatColor.WHITE));
-                TomeitoAPI.run(() -> TomeitoAPI.broadcastSound(Sound.BLOCK_NOTE_PLING, 1));
+                if (!e.getPlayer().hasPermission("mentions.mention-everyone")) return;
+                e.setMessage(e.getMessage().replaceAll("@everyone", String.valueOf(ChatColor.AQUA) + ChatColor.UNDERLINE + ChatColor.BOLD + "@everyone" + ChatColor.RESET + ChatColor.WHITE));
+                Bukkit.getOnlinePlayers().forEach(p -> p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 100000F, 1));
             } else if (s.equals("@admin")) { // @admin, mentions OPs (yellow color)
-                if (!e.getPlayer().hasPermission("mentions.admin")) return;
+                if (!e.getPlayer().hasPermission("mentions.mention-admin")) return;
                 e.setMessage(e.getMessage().replaceAll("@admin", ChatColor.GOLD + "@admin" + ChatColor.RESET + ChatColor.WHITE));
-                TomeitoAPI.run(() -> TomeitoAPI.getOnlinePlayers().filter(Player::isOp).forEach(p -> {
+                Bukkit.getOnlinePlayers().stream().filter(p -> p.hasPermission("mentions.is-admin")).forEach(p -> {
                     e.getRecipients().remove(p);
-                    String message = String.format(e.getFormat(), e.getPlayer().getName(), e.getMessage()).replaceAll("@admin", "" + ChatColor.GOLD + ChatColor.UNDERLINE + ChatColor.BOLD + "@admin" + ChatColor.RESET + ChatColor.WHITE);
+                    String message = String.format(e.getFormat(), e.getPlayer().getName(), e.getMessage()).replaceAll("@admin", String.valueOf(ChatColor.GOLD) + ChatColor.UNDERLINE + ChatColor.BOLD + "@admin" + ChatColor.RESET + ChatColor.WHITE);
                     p.sendMessage(message);
-                    p.playSound(p.getLocation(), Sound.BLOCK_NOTE_PLING, 100, 1);
-                }));
+                    p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 100, 1);
+                });
             } else if (s.startsWith("@")) { // @player (aqua color)
                 if (!e.getPlayer().hasPermission("mentions.mention")) return;
                 String p1 = s.replaceFirst("@", "");
@@ -41,11 +39,11 @@ public class Mentions extends JavaPlugin implements Listener {
                 if (p != null) {
                     e.getRecipients().remove(p);
                     e.setMessage(e.getMessage().replaceAll("@" + p1, ChatColor.AQUA + "@" + p1 + ChatColor.RESET + ChatColor.WHITE));
-                    String message = String.format(e.getFormat(), e.getPlayer().getName(), e.getMessage()).replaceAll("@" + p1, "" + ChatColor.AQUA + ChatColor.UNDERLINE + ChatColor.BOLD + "@" + p1 + ChatColor.RESET + ChatColor.WHITE);
+                    String message = String.format(e.getFormat(), e.getPlayer().getName(), e.getMessage()).replaceAll("@" + p1, String.valueOf(ChatColor.AQUA) + ChatColor.UNDERLINE + ChatColor.BOLD + "@" + p1 + ChatColor.RESET + ChatColor.WHITE);
                     p.sendMessage(message);
-                    p.playSound(p.getLocation(), Sound.BLOCK_NOTE_PLING, 100, 1);
+                    p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 100, 1);
                 }
             }
-        });
+        }
     }
 }
